17cd06b829e341304edd90fdaee9bca4cd449b20
Hassan Aguezoum <haguezoum@gmail.com>
2024-10-25 20:37:02 +0000

    add API Service

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,25 +1,23 @@
-const API = async (URL, headers) => {
-    // TODO : 
-    // should return a promise that fetch data from the URL using the headers provided
-    // should return the response as JSON
-    // should handle the error if the request failed and return the error message
-    // should use async/await to handle the promise returned by fetch
-    // should use try/catch to handle the error
+/**
+ * 
+ * This will take the endpoint URL, method and headers as input and return the response as JSON
+ * @param {string} URL 
+ * @param {string} metode
+ * @param {Object} headers 
+ * @returns {Promise}
+ */
 
+const API = async (URL, metode ,headers) => {
+    const URL = new URL(URL, "http://localhost:3000"); // Param 1 : URL, Param 2 : Base URL
     try {
-        const response = await fetch(URL, headers);
+        const response = await fetch(URL, {
+            method: metode,
+            headers: headers
+        });
         return response.json();
     } catch (error) {
-
+        console.log(`Error on fetch : ${error.message}`);
     }
 }
 
-
-
-// TODO :
-
-    // shoud use URL object to create URL SRC: https://developer.mozilla.org/en-US/docs/Web/API/URL
-
-    // should use fetch API to make the request , read more about fetch [https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API ], [https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch]
-
-    // should use async/await to handle the promise returned by fetch
+export default API;
