e80d48eb5d32effb005a95b5cb66834d75cb273e
Hassan aguezoum <haguezoum@gmail.com>
2024-11-02 11:20:23 +0100

    init loging page (test)

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,47 +1,88 @@
 let template = document.createElement("template");
 
-template.innerHTML =
-  `<div id="login-page" class="loginPage" > 
-    <form class="form shadow">
-      <div class="form-header">
-        lol
+template.innerHTML = /*html*/ `
+<div id="login-page" class="loginPage" > <!-- take the whole window size -->
+    <div  class="container">
+      <form class="form shadow">
+        <div class="form-header">
+          <span>
+            <img src="src/assets/images/42_logo.svg" alt="logo" />
+          </span>
+          <h2 class="title">Welcome back</h2>
+          <p class="slugan">Enter you email and password to access your account</p>
+        </div>
+        <div class="form-body">
+          <div class="form-group">
+              <div class="form-field">
+                <label for="email">Username</label>
+                <input type="email" class="email" id="email" name="email" placeholder="Username or email" autocomplete="email" required>
+                <p class="error" id="email-error" hidden>Error</p>
+              </div>
+              <div class="form-field">
+                <label for="password">Password</label>
+                <input type="password" class="password" id="password" name="password" placeholder="Password" autocomplete="current-password" required minlength="10">
+                <p class="error" id="password-error" hidden>Error</p>
+              </div>
+              <p class="password_forget">Forgot password?</p>
+              <div class="form-field">
+                <button class="btn_submit"  type="submit">Login</button>
+              </div>
+          </div>
+        </div>
+      </form>
+      <div class="form-footer">
+        <p>Or sign in with</p>
+        <div class="remote-login">
+            <button class="btn_42Network" role="button">
+               <img class="img_btn" src="src/assets/images/42_logo.svg" alt="42 Network"/>
+            </button>
+            <button class="btn_google" role="button">
+              <img  class="img_btn" src="src/assets/images/white-google-logo.png" alt="google"/>
+            </button>
+        </div>
+        <div class="signup">
+          <p>Don't have an account? <a href="/home">Sign up</a></p>
+        </div>
       </div>
-    </form>
-  </div>`;
-
-
-
-
-
+    </div>
+  </div>
+  `;
 
 class LOGIN extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    shadow.appendChild(template.content.cloneNode(true));
-    const linkElem = document.createElement('link');
-    linkElem.setAttribute('rel', 'stylesheet');
-    linkElem.setAttribute('href', 'src/assets/style/login-page.css');
-    shadow.appendChild(linkElem);
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.appendChild(template.content.cloneNode(true));
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", "src/assets/style/login-page.css");
+    this.shadow.appendChild(linkElem);
+    // this.shadow.childeNodes.forEach((e) => e.remove());
   }
 
   connectedCallback() {
     console.log("LOGIN is Connected");
+    console.log(this.shadow);
+    this.shadow.addEventListener("click", (e) => { 
+      e.preventDefault();
+      console.log(e.target);
+    });
   }
-  
+
   async disconnectedCallback() {
-    console.log('LOGIN is Disonnected');
+    console.log("LOGIN is Disonnected");
   }
 
   static get observedAttributes() {
-    return[/* array of attribute names to monitor for changes */];
+    return [
+      /* array of attribute names to monitor for changes */
+    ];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     // called when one of attributes listed above is modified
   }
-
 }
-customElements.define('login-page', LOGIN);
+customElements.define("login-page", LOGIN);
 
 export default LOGIN;
