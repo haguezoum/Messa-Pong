89cb0e6ece693de1492f2d9251be3dcc3a20cc7f
Hassan aguezoum <haguezoum@gmail.com>
2025-02-23 15:41:17 +0000

    add the base of profile page

--- UI/frontend/src/pages/profile.js
+++ UI/frontend/src/pages/profile.js
@@ -1,36 +1,78 @@
 let template = document.createElement("template");
 
-template.innerHTML = `<div id="profile-page" class="profile-page"> Hello from PROFILE... ! </div>`;
+template.innerHTML =
+  /*html*/
+  `<div id="profile-page" class="profile-page">
+      <div class="container">
+        <header-bar></header-bar>
+        <div class="profile-content">
+          <div class="profile-content-infos">
+            <div class="profile-content-infos-header">
+              <div class="profile-content-infos-header-username">
+                <h1>
+                  <span>Hi, </span>
+                  <span class="ownerName" data-username="haguezoum" onclick="app.state.currentPage='/home'">Hassan Aguezoum</span>
+                  <span class="wavingHand">👋🏽</span>
+                </h1>
+              </div>
+              <div class="profile-content-infos-message">
+                <p>Manage your details, view your tier status and change your password</p>
+              </div>
+            </div>
 
+            <section class="profile-content-infos-details">
+                <div class="profilePictureChanger">
+                   <div class="picturChanger">
+                        <img src="src/assets/images/charachters/bust-mask-13.svg" alt="" class="profilePicture">
+                        <div class="changePicture">
+                            <label for="file" class="changePictureIcon">
+                              <input type="file" class="file-input" id="file" />
+                              <ion-icon name="camera-outline" size="large" aria-hidden="true" aria-label="Change Personal Picture"></ion-icon>
+                            </label>
+                        </div>
+                   </div>
+                   <div class="userBasicInfo">
+                   <div class="userFullNmae">Hassan Aguezoum</div>
+                   <div class="userPhoneNumber">+212771426752</div>
+                   </div>
+                </div>
+                <div class="profileGeneralInfo">
+                  c
+                </div>
+                <div class="profileSecurity">
+                  A
+                </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    <cloud-moving cloudCount="20"></cloud-moving>
+  </div>`;
 
 class PROFILE extends HTMLElement {
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: 'open' });
-    this.shadow.appendChild(template.content.cloneNode(true));
-    const linkElem = document.createElement('link');
-    linkElem.setAttribute('rel', 'stylesheet');
-    linkElem.setAttribute('href', 'src/assets/style/profile-page.css');
+    this.shadow = this.attachShadow({ mode: "open" });
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", "src/assets/style/profile-page.css");
     this.shadow.appendChild(linkElem);
   }
 
   connectedCallback() {
-    console.log("PROFILE is Connected");
+    this.shadow.appendChild(template.content.cloneNode(true));
   }
-  
-  disconnectedCallback() {
-    console.log('PROFILE is Disonnected');
-  }
+
+  disconnectedCallback() {}
 
   static get observedAttributes() {
-    return[/* array of attribute names to monitor for changes */];
+    return [];
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
-    // called when one of attributes listed above is modified
-  }
+  attributeChangedCallback(name, oldValue, newValue) {}
 
+  // ------------------------------ Methods ------------------------------
 }
-customElements.define('profile-page', PROFILE);
+customElements.define("profile-page", PROFILE);
 
 export default PROFILE;
