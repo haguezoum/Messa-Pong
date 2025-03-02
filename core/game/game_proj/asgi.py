d12ad961a99abdd0cb5d559bc9b7506107dc076e
mbelouar <simobelouarraq5@gmail.com>
2025-03-02 17:38:45 +0000

    fix sessions for web sockets connection

--- core/game/game_proj/asgi.py
+++ core/game/game_proj/asgi.py
@@ -14,14 +14,20 @@ from channels.auth import AuthMiddlewareStack
 from game_app.routing import websocket_urlpatterns
 from channels.layers import get_channel_layer
 from channels.sessions import SessionMiddlewareStack
+from django.contrib.sessions.middleware import SessionMiddleware
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_proj.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': SessionMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            SessionMiddlewareStack(
+                URLRouter(
+                    websocket_urlpatterns
+                )
+            )
         )
     ),
 })
