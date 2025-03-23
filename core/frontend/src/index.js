507a795aafd3ebebfc7641841c900bdc64378878
Hassan aguezoum <haguezoum@gmail.com>
2025-03-23 09:21:57 +0000

    import all the component publicly

--- core/frontend/src/index.js
+++ core/frontend/src/index.js
@@ -2,3 +2,7 @@ import "./components/router-link.js";
 import "./components/cloud-moving.js";
 import "./components/loading-progress.js";
 import "./components/header-bar.js";
+import "./components/game-statsCard.js";
+import "./components/match-statsChart.js";
+import "./components/minutes-chart.js";
+import "./components/game-history-card.js";
